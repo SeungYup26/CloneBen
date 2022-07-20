d75202cc6db042c0c60998d21679a4210006cf50
U2V1bmdZdXA= <seungyup26@gmail.com>
2022-07-20 11:21:55 +0900

    Upload main files

---
+++ CloneBen/CloneBen/CloneBen.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace CloneBen
+{
+    partial class CloneBen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.killEnemy = new System.Windows.Forms.Timer(this.components);
+            this.showBen = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // killEnemy
+            // 
+            this.killEnemy.Enabled = true;
+            this.killEnemy.Interval = 200;
+            this.killEnemy.Tick += new System.EventHandler(this.killEnemy_Tick);
+            // 
+            // showBen
+            // 
+            this.showBen.Enabled = true;
+            this.showBen.Interval = 200;
+            this.showBen.Tick += new System.EventHandler(this.showBen_Tick);
+            // 
+            // CloneBen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CloneBen.Properties.Resources.Ben1;
+            this.ClientSize = new System.Drawing.Size(170, 294);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CloneBen";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.CloneBen_FormClosing);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.CloneBen_MouseDown);
+            this.MouseEnter += new System.EventHandler(this.CloneBen_MouseEnter);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.CloneBen_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer killEnemy;
+        private System.Windows.Forms.Timer showBen;
+    }
+}
+
---
+++ CloneBen/CloneBen/CloneBen.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CloneBen
+{
+    public partial class CloneBen : Form
+    {
+        public CloneBen()
+        {
+            InitializeComponent();   
+        }
+
+        // Moving CloneBen to random position
+        private void MoveToRandPos()
+        {
+            Random random = new Random();
+            Random random2 = new Random();
+
+            int screenWidth = Screen.PrimaryScreen.Bounds.Width - 200;
+            int screenHeight = Screen.PrimaryScreen.Bounds.Height - 200;
+            int xPosition = random.Next(0, screenWidth);
+            int yPosition = random2.Next(0, screenHeight);
+            base.Location = new Point(xPosition, yPosition);
+        }
+
+        // Disabling event to close program
+        private void CloneBen_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        // Avoiding a mouse-pointer
+        private void CloneBen_MouseEnter(object sender, EventArgs e)
+        {
+            MoveToRandPos();
+        }
+        private void CloneBen_MouseMove(object sender, MouseEventArgs e)
+        {
+            MoveToRandPos();
+        }
+        private void CloneBen_MouseDown(object sender, MouseEventArgs e)
+        {
+            MoveToRandPos();
+        }
+
+        // Killing enemy processes
+        private void killEnemy_Tick(object sender, EventArgs e)
+        {
+            foreach (Process process in Process.GetProcesses())
+            {
+                string procName = process.ProcessName.ToLower();
+                if (procName == "taskmgr" || procName == "cmd")
+                {
+                    process.Kill();
+                    MessageBox.Show("I hate it", ":(");
+                    break;
+                }
+            }
+        }
+
+        // Show CloneBen
+        private void showBen_Tick(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            this.Focus();
+            this.BringToFront();
+            this.TopMost = false;
+        }
+    }
+}
